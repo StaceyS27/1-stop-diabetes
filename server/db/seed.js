4847f49a8aad18fdf56463d0e1f1d701f1873ed2
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-02 16:39:47 -0400

    seeding facility and doctor tables

--- server/db/seed.js
+++ server/db/seed.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const SALT_COUNT = 10;
 
 async function main() {
-    //seeding the specialty table
+    // seeding the specialty table
     await prisma.specialty.upsert({
         where: {specialtyName: 'Podiatry'},
         update: {},
@@ -59,6 +59,82 @@ async function main() {
             recommendedFrequency: 12
         }
     });
+
+    // seeding facility table 
+    await prisma.facility.upsert({
+        where: {name: 'The Brookhaven Center'},
+        update: {},
+        create: {
+            name: 'The Brookhaven Center',
+            address: '1059 Nelson Avenue',
+            city: 'Bronx',
+            state: 'New York',
+            zipcode: '10453',
+            doctor: {
+                create: [
+                    {
+                        name: 'Dr. Samuel Smith',
+                        phoneNumber: '(718) 293-8858',
+                        specialtyId: 1
+                    },
+                    {
+                        name: 'Dr. Alejandro Rodriguez',
+                        phoneNumber: '(718) 293-8858',
+                        specialtyId: 2
+                    }
+                ]
+            }
+        }
+    });
+
+    await prisma.facility.upsert({
+        where: {name: 'Summerville Family Center'},
+        update: {},
+        create: {
+            name: 'Summerville Family Center',
+            address: '91-31 Woodhaven Blvd',
+            city: 'Queens',
+            state: 'New York',
+            zipcode: '11421',
+        }
+    });
+
+    await prisma.facility.upsert({
+        where: {name: 'Advance Care Physicians'},
+        update: {},
+        create: {
+            name: 'Advance Care Physicians',
+            address: '2255 Myrtle Street',
+            city: 'Brooklyn',
+            state: 'New York',
+            zipcode: '11201',
+        }
+    });
+
+    await prisma.facility.upsert({
+        where: {name: 'All Smiles Clinic'},
+        update: {},
+        create: {
+            name: 'All Smiles Clinic',
+            address: '2905 St. Nicholas Avenue',
+            city: 'New York',
+            state: 'New York',
+            zipcode: '10036'
+        }
+    })
+
+    // seeding doctor table
+    await prisma.doctor.upsert({
+        where: {name: 'Dr. Xing Xu'},
+        update: {},
+        create: {
+            name: 'Dr. Xing Xu',
+            phoneNumber: '(718) 344-9158',
+            specialtyId: 2,
+            facilityId: 2
+        }
+    });
+
 
 }
 main()
