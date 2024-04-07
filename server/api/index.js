57397493bf0193bf050b52d4158eadf6e4492900
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-07 13:57:08 -0400

    middleware to set req.user written

--- server/api/index.js
+++ server/api/index.js
@@ -11,6 +11,45 @@ apiRouter.get('/', (req, res, next) => {
     res.send('this is the apiRouter')
 });
 
+//set "req.user"
+apiRouter.use(async function (req, res, next) {
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+
+    if(!auth) {
+        next();
+    } else if (auth.startsWith(prefix)) {
+        const token = auth.slice(prefix.length);
+
+        try {
+            const payload = jwt.verify(token, JWT_SECRET);
+            const id = payload.id
+
+            if(id) {
+                req.user = await prisma.user.findUnique({
+                    where: {
+                        id: id
+                    }
+                })
+                next();
+            } else {
+                res.status(401).send({
+                    message: 'Authorization token malformed'
+                });
+            }
+            console.log(req.user)
+        } catch (error) {
+            next({error})
+        }
+    } else {
+        next({
+            name: 'AuthorizationHeaderError',
+            message: 'Authorization token must start with ' + prefix
+        })
+    }
+});
+
+//subrouters of apiRouter
 
 
 module.exports = apiRouter
