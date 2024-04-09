63debd65e625e063990add4ee9e774f0361f8632
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-09 12:05:49 -0400

    created get all doctors with specialty and facility information endpoint

--- server/api/doctors.js
+++ server/api/doctors.js
@@ -8,6 +8,23 @@ doctorsRouter.get('/test', async function (req, res, next) {
 });
 
 // GET /api/doctors - get all doctors with specialty & facility
+doctorsRouter.get('/', async function (req, res, next) {
+    try {
+        const allDoctors = await prisma.doctor.findMany({
+            include: {
+                specialty: true,
+                facility: true,
+            }
+        });
+
+        res.send(allDoctors)
+
+    } catch {
+        next({
+            message: 'Unable to get all doctors.'
+        })
+    }
+});
 
 // GET /api/doctors/:doctorId
 
