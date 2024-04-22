21c8eb1050520bb3f872be2f47ef7f60bfdf13d0
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-21 22:26:55 -0400

    query and mutation functions created in redux for user endpoints

---
+++ client/Redux/1-stop-diabetesApi.js
@@ -0,0 +1,49 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const stopDiabetesApi= createApi({
+    reducerPath: "stopDiabetesApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "/",
+
+        prepareHeaders: (headers, {getState}) => {
+            headers.set("Content-Type", "application/json");
+            const {token} = getState();
+            if(token) {
+                headers.set("authorization", `Bearer ${token}`)
+            }
+
+            return headers;
+        },
+    }),
+
+    endpoints: (builder) => ({
+        //-------------------USER ENDPOINTS-----------------------//
+        // GET /api/users/me - get single user information by id (req.user.id)
+        getUser: builder.query({
+            query: () => "/api/users/me",
+            providesTags: ["user"]
+        }),
+        // POST /api/users/register - register new user
+        registerUser: builder.mutation({
+            query: (user) => ({
+                url: "/api/users/register",
+                method: "POST",
+                body: user, 
+            }),
+            invalidatesTag: ["user"]
+        }),
+        // POST /api/users/login - login existing user
+        loginUser: builder. mutation({
+            query: (user) => ({
+                url: "/api/users/login",
+                method: "POST",
+                body: user,
+            }),
+            invalidatesTags: ["user"]
+        }),
+
+        //-----------------DOCTOR ENDPOINTS------------------------//
+        // GET /api/doctors- get all doctors with specialty and facility
+        
+    })
+})
